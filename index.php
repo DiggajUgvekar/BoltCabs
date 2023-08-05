b46ded3b8d5db61dc8911d292e76fecfee63c5f6
Diggaj Ugvekar <diggajugvekar@gmail.com>
2023-08-05 22:05:59 +0530

    First Commit with main page

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <link rel="stylesheet" href="style.css">
+    <title>BoltCabs</title>
+<body>
+    <header>
+        <a href="index.php" class = "logo">BoltCabs</a>
+        <ul class="nav-page">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+        <ul>
+        <li><a href="login.php" class="login">Log In</a></li>
+        <li><a href="signup.php" class="signup">Sign Up</a></li>
+        </ul>
+    </header>
+    
+    <section class="banner">
+        
+        <div class="image-container" >
+            <h1 class="slogan">Rent A Cab Now!</h1>
+            <a href="login.php" class="rentnow">Get Started</a>
+        </div>
+    </section>
+
+<?php
+// echo "My first PHP script!";
+?>
+
+</body>
+</html>
